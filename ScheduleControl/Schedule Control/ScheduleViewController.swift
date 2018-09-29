16f7efc47af898320b636f2801e0f0a25a10f051
parthahuj@gmail.com <parthahuj@gmail.com>
2018-09-29 19:25:04 -0400

    first : )

---
+++ ScheduleControl/Schedule Control/ScheduleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schedule Control
+//
+//  Created by Parth Ahuja on 9/19/18.
+//  Copyright © 2018 Parth Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
